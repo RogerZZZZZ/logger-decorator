bc41aa009ba187b48cd1a4aae3da84ffdafbdc9c
RogerZZZZZ <501841352@qq.com>
2018-12-04 22:51:48 +0800

    add test for FuncLogger

---
+++ test/unit/func-logger-decorator.spec.ts
@@ -0,0 +1,119 @@
+import { ClassLogger, FuncLogger, DisableLogger } from '../../dist/lib/logger-decorator'
+import { ConsoleCollector } from './lib/console-collector'
+
+describe('Test FuncLogger', () => {
+  it('Test config override', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message',
+      logReturn: true,
+      withParams: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      @FuncLogger({
+        withParams: false,
+      })
+      testFunction1() {
+        return this.field1
+      }
+
+      @FuncLogger({
+        logReturn: false,
+        beginMessage: 'test function2'
+      })
+      testFunction2() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testFunction1()
+    basic.testFunction2()
+    const history = collector.history()
+    expect(history).toEqual([
+      '[Basic#testFunction1] START',
+      '    Begin Message',
+      '    Return Value: basic',
+      '[Basic#testFunction1] END',
+      '[Basic#testFunction2] START',
+      '    test function2',
+      '    Parameters: []',
+      '[Basic#testFunction2] END'])
+  })
+})
+
+describe('Test DisableLogger', () => {
+  it('Test DisableLogger decorator', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message',
+      logReturn: true,
+      withParams: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      @FuncLogger({
+        withParams: false,
+      })
+      testFunction1() {
+        return this.field1
+      }
+
+      @DisableLogger()
+      testFunction2() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testFunction1()
+    basic.testFunction2()
+    const history = collector.history()
+    expect(history).toEqual([
+      '[Basic#testFunction1] START',
+      '    Begin Message',
+      '    Return Value: basic',
+      '[Basic#testFunction1] END'])
+  })
+
+  it('Test use FuncLogger and DisableLogger meantime', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message',
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      @FuncLogger({
+        withParams: false,
+      })
+      @DisableLogger()
+      testFunction1() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testFunction1()
+    const history = collector.history()
+    expect(history.length).toBe(0)
+  })
+})
