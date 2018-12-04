4c370ee54acd283c743bb5373a05733f57204789
RogerZZZZZ <501841352@qq.com>
2018-12-04 22:56:52 +0800

    add Logger Decorator test

--- test/unit/func-logger-decorator.spec.ts
+++ test/unit/func-logger-decorator.spec.ts
@@ -1,4 +1,9 @@
-import { ClassLogger, FuncLogger, DisableLogger } from '../../dist/lib/logger-decorator'
+import {
+  ClassLogger,
+  FuncLogger,
+  DisableLogger,
+  Logger,
+} from '../../dist/lib/logger-decorator'
 import { ConsoleCollector } from './lib/console-collector'
 
 describe('Test FuncLogger', () => {
@@ -115,5 +120,36 @@ describe('Test DisableLogger', () => {
     basic.testFunction1()
     const history = collector.history()
     expect(history.length).toBe(0)
+  })
+})
+
+describe('Test Logger Decorate', () => {
+  it('Test without ClassDecorator', () => {
+    const collector = new ConsoleCollector()
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      @Logger({
+        withParams: true,
+        logReturn: true,
+        withTime: false,
+        log: collector.logger.bind(collector)
+      })
+      testFunction1(param: string) {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testFunction1('param')
+    const history = collector.history()
+    expect(history).toEqual([
+      '[Basic#testFunction1] START',
+      '    Parameters: [param: param]',
+      '    Return Value: basic',
+      '[Basic#testFunction1] END'])
   })
 })
