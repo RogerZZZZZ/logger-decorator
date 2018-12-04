4d51e57b26fc29d73e6a16e24f9ccf3d5b71f566
RogerZZZZZ <501841352@qq.com>
2018-12-04 21:42:45 +0800

    add class decorator test

--- test/unit/class-decorator.spec.ts
+++ test/unit/class-decorator.spec.ts
@@ -1,29 +1,110 @@
-import { ClassLogger } from '../../dist/lib/logger-decorator'
+import { ClassLogger } from '../../dist/lib/ogger-decorator'
 import { ConsoleCollector } from './lib/console-collector'
 
 
-describe('Class Logger Decorator', () => {
-  it('should bind logger', () => {
-
+describe('Test ClassLogger', () => {
+  it('Test basic class decorator', () => {
     const collector = new ConsoleCollector()
 
     @ClassLogger({
-      classProperties: true,
-      log: collector.logger.bind(collector)
+      log: collector.logger.bind(collector),
+      withTime: false,
     })
-    class Test {
+    class Basic {
       private field1: string
       constructor(field1: string) {
         this.field1 = field1
       }
-      test() {
-        //
+
+      testGetField() {
+        return this.field1
       }
     }
 
-    const test = new Test('111')
-    test.test()
-    console.log(collector.history())
-    // console.printHistory()
+    const basic = new Basic('basic')
+    basic.testGetField()
+    expect(collector.history()).toEqual([
+      '[Basic#testGetField] START',
+      '[Basic#testGetField] END'
+    ])
   })
+
+  it('Test with class propertities', () => {
+    const collector = new ConsoleCollector()
+
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      classProperties: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testGetField() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testGetField()
+    expect(collector.history()).toEqual([
+      '[Basic#testGetField] START',
+      '    Class Properties: [field1: basic]',
+      '[Basic#testGetField] END'
+    ])
+  })
+
+  it('Test with duration', () => {
+    const collector = new ConsoleCollector()
+
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      duration: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testDuration() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testDuration()
+    const history = collector.history()
+    expect(history.length).toBe(3)
+    expect(history[1]).toMatch('Function spent: ')
+  })
+
+  it('Test disable option', () => {
+    const collector = new ConsoleCollector()
+
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      duration: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testDisable() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testDisable()
+    expect(collector.history().length).toBe(0)
+  })
+
 })
