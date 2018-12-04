70b1d1033d293c9e7bb5e8ebc6df07a4583b82d4
RogerZZZZZ <501841352@qq.com>
2018-12-04 22:36:20 +0800

    add more cases for class decorator test

--- test/unit/class-decorator.spec.ts
+++ test/unit/class-decorator.spec.ts
@@ -1,11 +1,10 @@
-import { ClassLogger } from '../../dist/lib/ogger-decorator'
+import { ClassLogger } from '../../dist/lib/logger-decorator'
 import { ConsoleCollector } from './lib/console-collector'
 
 
 describe('Test ClassLogger', () => {
   it('Test basic class decorator', () => {
     const collector = new ConsoleCollector()
-
     @ClassLogger({
       log: collector.logger.bind(collector),
       withTime: false,
@@ -31,7 +30,6 @@ describe('Test ClassLogger', () => {
 
   it('Test with class propertities', () => {
     const collector = new ConsoleCollector()
-
     @ClassLogger({
       log: collector.logger.bind(collector),
       withTime: false,
@@ -59,7 +57,6 @@ describe('Test ClassLogger', () => {
 
   it('Test with duration', () => {
     const collector = new ConsoleCollector()
-
     @ClassLogger({
       log: collector.logger.bind(collector),
       withTime: false,
@@ -85,11 +82,11 @@ describe('Test ClassLogger', () => {
 
   it('Test disable option', () => {
     const collector = new ConsoleCollector()
-
     @ClassLogger({
       log: collector.logger.bind(collector),
       withTime: false,
       duration: true,
+      disable: true,
     })
     class Basic {
       private field1: string
@@ -107,4 +104,165 @@ describe('Test ClassLogger', () => {
     expect(collector.history().length).toBe(0)
   })
 
+  it('Test withParams', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      withParams: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testWithParams(param1: string, param2: string) {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testWithParams('param1', 'param2')
+    expect(collector.history()).toEqual([
+      '[Basic#testWithParams] START',
+      '    Parameters: [param1: param1,param2: param2]',
+      '[Basic#testWithParams] END',
+    ])
+  })
+
+  it('Test logReturn', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      logReturn: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testWithParams(param1: string, param2: string) {
+        return this.field1 + param1 + param2
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testWithParams('param1', 'param2')
+    expect(collector.history()).toEqual([
+      '[Basic#testWithParams] START',
+      '    Return Value: basicparam1param2',
+      '[Basic#testWithParams] END',
+    ])
+  })
+
+  it('Test beginMessage', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message'
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testWithParams(param1: string, param2: string) {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testWithParams('param1', 'param2')
+    expect(collector.history()).toEqual([
+      '[Basic#testWithParams] START',
+      '    Begin Message',
+      '[Basic#testWithParams] END',
+    ])
+  })
+})
+
+describe('Test ClassLogger In other occasion', () => {
+  it('Test whole options', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message',
+      logReturn: true,
+      duration: true,
+      withParams: true,
+      classProperties: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testAllOptions(param1: string, param2: string) {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testAllOptions('param1', 'param2')
+    const history = collector.history()
+    expect(history.length).toBe(7)
+    const duration = history.splice(4, 1)
+    expect(history).toEqual([
+      '[Basic#testAllOptions] START',
+      '    Begin Message',
+      '    Class Properties: [field1: basic]',
+      '    Parameters: [param1: param1,param2: param2]',
+      '    Return Value: basic',
+      '[Basic#testAllOptions] END',
+    ])
+    expect(duration).toMatch('Function spent: ')
+  })
+
+  it('Test multi functions', () => {
+    const collector = new ConsoleCollector()
+    @ClassLogger({
+      log: collector.logger.bind(collector),
+      withTime: false,
+      beginMessage: 'Begin Message',
+      logReturn: true,
+      withParams: true,
+    })
+    class Basic {
+      private field1: string
+      constructor(field1: string) {
+        this.field1 = field1
+      }
+
+      testFunction1(param1: string, param2: string) {
+        return param1 + param2
+      }
+
+      testFunction2() {
+        return this.field1
+      }
+    }
+
+    const basic = new Basic('basic')
+    basic.testFunction1('param1', 'param2')
+    basic.testFunction2()
+    expect(collector.history()).toEqual([
+      '[Basic#testFunction1] START',
+      '    Begin Message',
+      '    Parameters: [param1: param1,param2: param2]',
+      '    Return Value: param1param2',
+      '[Basic#testFunction1] END',
+      '[Basic#testFunction2] START',
+      '    Begin Message',
+      '    Parameters: []',
+      '    Return Value: basic',
+      '[Basic#testFunction2] END',
+    ])
+  })
 })
